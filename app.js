f5e0bbce9ea088a2f881496bf9c21d8cbe7400e4
ZXINNATTAPAT <106763926+ZXINNATTAPAT@users.noreply.github.com>
2023-03-11 01:13:49 +0700

    i fix xampp

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'my-db'
+    database: 'mydb'
   });
   
 app.post('/register',jsonParser, function (req, res, next) {
             bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
             connection.execute(
-            'INSERT INTO users (email,password,fname,lname) VALUES (?,?,?,?)',
-            [req.body.email, hash, req.body.fname, req.body.lname],
+            'INSERT INTO users (Tel,password,fname,lname) VALUES (?,?,?,?)',
+            [req.body.Tel, hash, req.body.fname, req.body.lname],
             function(err, results, fields) {
                 if(err){
                     res.json({status : 'error',message :err})
@@ -37,15 +37,15 @@ app.post('/register',jsonParser, function (req, res, next) {
 
 app.post('/login',jsonParser,function(req,res,next){
     connection.execute(
-        'SELECT * FROM users WHERE email=?',
-        [req.body.email],
+        'SELECT * FROM users WHERE Tel=?',
+        [req.body.Tel],
         function(err, users, fields) {
             if(err) { res.json({status : 'error',message :err}); return }
             if(users.length == 0) {res.json({status:'error', message:'No user found'}); return }
 
             bcrypt.compare(req.body.password, users[0].password, function(err, isLogin) {
                 if(isLogin){
-                    var token = jwt.sign({ email: users[0].email }, secret, { expiresIn: '1h' });
+                    var token = jwt.sign({ Tel: users[0].Tel }, secret, { expiresIn: '1h' });
                     res.json({status:'ok',message:'login success',token})
                     
                 }
